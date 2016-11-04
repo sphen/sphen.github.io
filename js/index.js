2219019a0887b6e39f9df574542c056a38ef8d70
sphen <sphencode@gmail.com>
2016-11-04 15:06:49 -0600

    Small Update
    Tweaks

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
+// Add smooth scrolling to all links inside a navbar
+
+
+
+/* Solution 1
+
 // Add scrollspy.js
 $('body').scrollspy({
   target: ".navbar",
   offset: 0
 });
 
-// Add smooth scrolling to all links inside a navbar
-$("#navbar a").on('click', function(event) {
+$("#navbar a").on('click', function() {
 
   event.preventDefault();
 
@@ -13,8 +18,43 @@ $("#navbar a").on('click', function(event) {
 
   $('html, body').animate({
     scrollTop: $(hash).offset().top
-  }, 800, function() {
-
-    window.location.hash = hash;
-  });
+  }, 800, 'swing');
 });
+*/
+
+/* Solution 2
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});*/
+
+
+
+//third
+
+$('a[href*="#"]:not([href="#"])').not('#port-carousel a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+$('body').scrollspy({ target: '#navbar' });
